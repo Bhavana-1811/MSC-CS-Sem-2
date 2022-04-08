45de6b16d8afbe3e8bdec5a7c77ee1055759da18
Bhavana-1811 <mishra.a.bhavana@gmail.com>
2022-04-08 07:53:50 +0530

    Added BIBD Practicals

---
+++ BIBD/Practicals/KMoments.java
@@ -0,0 +1,49 @@
+media_id = {
+    int
+}
+1511975333301714944
+processing_info = {
+    dict: 2
+} {
+    'state': 'succeeded',
+    'progress_percent': 100
+}
+total_bytes = {
+    int
+}
+1570024
+uri = {
+    str
+}
+'https://stattachment.blob.core.windows.net/blobstore/UnoApp/39/2022/4/6/39668279/file_example_MP4_480_1_5MG.mp4'
+video_filename = {
+    str
+}
+'E:\\repos\\message-processor\\temp\\file_example_MP4_480_1_5MG.mp4'
+{
+    'Date': 'Thu, 07 Apr 2022 08:03:09 GMT',
+    'Pragma': 'no-cache',
+    'Server': 'tsa_k',
+    'Status': '403 Forbidden',
+    'Expires': 'Tue, 31 Mar 1981 05:00:00 GMT',
+    'Set-Cookie': 'guest_id_marketing=v1%3A164931858981106083; Max-Age=63072000; Expires=Sat, 06 Apr 2024 08:03:09 GMT; Path=/; Domain=.twitter.com; Secure; SameSite=None, guest_id_ads=v1%3A164931858981106083; Max-Age=63072000; Expires=Sat, 06 Apr 2024 08:03:09 GMT; Path=/; Domain=.twitter.com; Secure; SameSite=None, personalization_id="v1_Vk6baN9N+gvDuQsXsMw16A=="; Max-Age=63072000; Expires=Sat, 06 Apr 2024 08:03:09 GMT; Path=/; Domain=.twitter.com; Secure; SameSite=None, lang=en; Path=/, guest_id=v1%3A164931858981106083; Max-Age=63072000; Expires=Sat, 06 Apr 2024 08:03:09 GMT; Path=/; Domain=.twitter.com; Secure; SameSite=None',
+    'Content-Type': 'application/json;charset=utf-8',
+    'Cache-Control': 'no-cache, no-store, must-revalidate, pre-check=0, post-check=0',
+    'Last-Modified': 'Thu, 07 Apr 2022 08:03:09 GMT',
+    'X-Transaction': '2c1e83b88d76577d',
+    'Content-Length': '162',
+    'X-Access-Level': 'read-write-directmessages',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'X-Xss-Protection': '0',
+    'Content-Disposition': 'attachment; filename=json.json',
+    'X-Content-Type-Options': 'nosniff',
+    'X-Twitter-Response-Tags': 'BouncerCompliant',
+    'Strict-Transport-Security': 'max-age=631138519',
+    'X-Response-Time': '192',
+    'X-Connection-Hash': 'dff32ed8f7c5f067f6f461fa3644fc5ab3c412536cc284b93fe2c0cdd9f86fb6',
+    'Via': '1.1 google',
+    'Alt-Svc': 'clear'
+}
+
+
+1511981281663655936
